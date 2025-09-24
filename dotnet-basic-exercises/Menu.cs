dfc32876aed8df91061ae5b9b148e714748ddaa5
kaylee paez <kayleepaez1121388@correo.itm.edu.co>
2025-09-23 22:06:53 -0500

    Exercise1 completed

---
+++ dotnet-basic-exercises/Menu.cs
@@ -0,0 +1,30 @@
+namespace DotnetBasicExercises
+{
+    public class Menu
+    {
+        public static void ShowMenu()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Seleccione una opción:");
+            Console.WriteLine("1. Ejercicio 1: Elevar un número positivo al cuadrado.");
+            Console.WriteLine("2. Ejercicio 2: Devolver número mayor dupliacado triplicado.");
+            Console.WriteLine("3. Ejercicio 3: Devolver raíz o cuadrado de un número.");
+            Console.WriteLine("4. Ejercicio 4: Calcular perímetro de un círculo.");
+            Console.WriteLine("5. Ejercicio 5: Días laborales.");
+            Console.WriteLine("6. Ejercicio 6: Calculadora de impuestos.");
+            Console.WriteLine("7. Ejercicio 7: Calculadora de residuos.");
+            Console.WriteLine("8. Ejercicio 8: Sumar pares entre 1 y 50.");
+            Console.WriteLine("9. Ejercicio 9: Calcular diferencia entre fracciones.");
+            Console.WriteLine("10. Ejercicio 10: Mostrar la longitud de una palabra.");
+            Console.WriteLine("11. Ejercicio 11: Calcular promedio de 4 números.");
+            Console.WriteLine("12. Ejercicio 12: Calcular el menor de 5 números.");
+            Console.WriteLine("13. Ejercicio 13: Devolver cantidad de vocales en una palabra.");
+            Console.WriteLine("14. Ejercicio 14: Calcular factorial de un número.");
+            Console.WriteLine("15. Ejercicio 15: Verificar si un número pertenece al rango 10 - 20.");
+            Console.WriteLine("16. Salir");
+
+            Console.WriteLine("");
+            Console.Write("Ingrese el número de la opción deseada: ");
+        }
+    }
+}
