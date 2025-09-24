1d25a3154f341c1605efe0691f0bebdc962cb177
kaylee paez <kayleepaez1121388@correo.itm.edu.co>
2025-09-24 15:58:45 -0500

    Exercise14 completed!

---
+++ dotnet-basic-exercises/exercises/Exercise14.cs
@@ -0,0 +1,39 @@
+namespace DotnetBasicExercises
+{
+    public class Exercise14
+    {
+        public static void Run()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Ingresa un número: ");
+                    int num = int.Parse(Console.ReadLine());
+                    Console.WriteLine("");
+
+                    int factorial = 1;
+
+                    if (num >= 0)
+                    {
+                        for (int i = num; i > 0; i--)
+                            factorial *= i;
+
+                        Console.WriteLine($"El factorial de {num} es: {factorial}");
+
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("el número debe ser un entero positivo.");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("El valor ingresado debe ser un entero positivo.");
+                }
+            }
+        }
+    }
+}
