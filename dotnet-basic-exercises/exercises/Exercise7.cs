76aa7007b8e52f11f9cf756e66de6b1261e88281
kaylee paez <kayleepaez1121388@correo.itm.edu.co>
2025-09-24 10:55:21 -0500

    Exercise7 completed!

---
+++ dotnet-basic-exercises/exercises/Exercise7.cs
@@ -0,0 +1,36 @@
+namespace DotnetBasicExercises
+{
+    public class Exercise7
+    {
+        public static void Run()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Ingresa el primer número: ");
+                    double dividend = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingresa el segundo número: ");
+                    double divisor = double.Parse(Console.ReadLine());
+                    Console.WriteLine("");
+
+                    if (divisor != 0)
+                    {
+                        int remainder = (int)(dividend % divisor);
+                        Console.WriteLine($"El residuo de la división {dividend}/{divisor} es: {remainder}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El divisor debe ser diferente de cero.");
+                    }
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Uno de los valores ingresados es inválido. Por favor, intenta de nuevo.");
+                }
+            }
+        }
+    }
+}
